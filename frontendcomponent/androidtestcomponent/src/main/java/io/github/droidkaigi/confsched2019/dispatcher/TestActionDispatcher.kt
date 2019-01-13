c51c6ce2f0a94e3044301f7f0868d0990488e463
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-01-13 23:48:44 +0900

    Removed my implementation of immediate context dispatcher

--- frontendcomponent/androidtestcomponent/src/main/java/io/github/droidkaigi/confsched2019/dispatcher/TestActionDispatcher.kt
+++
@@ -1,9 +0,0 @@
-package io.github.droidkaigi.confsched2019.dispatcher
-
-import androidx.annotation.WorkerThread
-import kotlinx.coroutines.asCoroutineDispatcher
-
-object TestActionDispatcher {
-    @WorkerThread
-    operator fun invoke() = CurrentThreadExecutorService().asCoroutineDispatcher()
-}
